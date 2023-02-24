b955850d7d8e62e44b89b2bb2144b7ba3fc082bd
K3rNKub <111554758+K3rNKub@users.noreply.github.com>
2023-02-24 11:57:16 +0700

    Add files via upload

---
+++ EX53.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+
+public class EX53 {
+    Scanner input = new Scanner(System.in);
+    
+}
---
+++ Ex1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Ex1{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter num1");
+        int num1 = input.nextInt();
+        System.out.println("Enter num2");
+        int num2 = input.nextInt();
+        System.out.println("Enter num3");
+        int num3 = input.nextInt();
+        System.out.println("Enter num4");
+        int num4 = input.nextInt();
+        System.out.println("Enter num5");
+        int num5 = input.nextInt();
+
+        int max = num1;
+        if(max < num2){
+        }
+        if (max < num3) {
+        }
+        if(max < num4) {
+        }
+        if (max < num5) {
+        }
+    }
+}
